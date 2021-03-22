73118263d9f79274c6d0102698ef09c6995354ec
IldarArsl <arslambekov63@gmail.com>
2021-03-22 22:50:03 +0300

    write linked stack

--- src/linked_stack.cpp
+++ src/linked_stack.cpp
@@ -5,19 +5,38 @@
 namespace itis {
 
 void LinkedStack::Push(Element e) {
+    SinglyNode* new_node = new SinglyNode(e, top_);
+    top_ = new_node;
   // TODO: напишите здесь свой код ...
+  size_++;
 }
 
 void LinkedStack::Pop() {
   if (top_ == nullptr) {
     throw std::logic_error("cannot pop out from empty stack");
   }
-
+  SinglyNode* node_to_delete = top_;
+  top_ = top_->next;
+  delete node_to_delete;
+  size_--;
   // TODO: напишите здесь свой код ...
 }
 
 void LinkedStack::Clear() {
   // TODO: напишите здесь свой код ...
+  if (size_ >1 ) {
+      SinglyNode* current_node;
+      SinglyNode* next_node = top_->next;
+  for (int i = 1; i < size_; i++) {
+      current_node = next_node;
+      next_node = current_node->next;
+      delete current_node;
+  }
+  }
+  if (size_!= 0)
+    delete top_;
+  size_ = 0;
+  top_ = nullptr;
 }
 
 // === РЕАЛИЗОВАНО ===
